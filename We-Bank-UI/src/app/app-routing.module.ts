15e1d461e55ef64360b1e86e24ac0dd149507402
Venu Katike <venukatike02@gmail.com>
2024-04-14 19:04:36 +0530

    added sign-in  component

--- We-Bank-UI/src/app/app-routing.module.ts
+++ We-Bank-UI/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainNavbarComponent } from './components/main-navbar/main-navbar.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
 const routes: Routes = [
-  {path:'',component:MainNavbarComponent}
+  {path:'',component:MainNavbarComponent},
+  {path:'sign-in',component:SignInComponent}
 ];
 
 @NgModule({
--- We-Bank-UI/src/app/app.module.ts
+++ We-Bank-UI/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainNavbarComponent } from './components/main-navbar/main-navbar.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavbarComponent
+    MainNavbarComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
